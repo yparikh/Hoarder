4b65f157f8ef6b702b9bf9f33bdb4dfc1891be00
yparikh <yparikh95@gmail.com>
2022-11-04 22:20:35 -0400

    Implemented Dark Mode + added App Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import Header from './components/Header'
+
+
+
+
 function App() {
   return (
     <div className="App">
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import "beercss";
+import "material-dynamic-colors";
+
+
+
+const DarkModeButton = () => {
+
+    const [toggle, setToggle] = useState("light");
+
+    const mode = () => {
+      const newMode = window.ui("mode") === "dark"
+          ? "light"
+          : "dark";
+      setToggle(window.ui("mode", newMode));
+  }
+
+    useEffect(() => {
+        mode();
+    })
+
+    return (
+        <label className="switch" onClick={()=> mode()}>
+            <input type="checkbox"/>
+            <span>
+                <i>dark_mode</i>
+            </span>
+        </label>
+    )
+}
 
 const Header = () => {
-  return (
-    <div>
-      <h1> our header </h1>
-    </div>
-  )
+    return (
+        <header className="fill">
+            <nav>
+                <button className="circle transparent">
+                    <i>menu</i>
+                </button>
+                <h5 className="max center-align">Hoarder</h5>
+                <DarkModeButton/>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
